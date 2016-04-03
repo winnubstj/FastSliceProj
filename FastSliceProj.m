356fb720194b549dab252fe74a61fa68f27f7c0b
winnubstj <winnubstj@janelia.hhmi.org>
2016-04-03 16:57:07 -0400

    Some fixes of duplicate removal

--- FastSliceProj.m
+++ FastSliceProj.m
@@ -1,26 +1,28 @@
 function [ ] = FastSliceProj(  )
 %FastSliceProj Takes JSON file from Acquisition dashboard and generates projection off all stacks belonging to same slice.
-% 
+% Dependency: fastProtoBuf
 
-tileDir = 'Y:\mousebrainmicro\acquisition\3-21-2016\Data\'
+tileDir = 'Y:\mousebrainmicro\acquisition\3-21-2016\Data\';
+reqZ = 172;
 
 %% load and convert Json file to .mat
+fprintf('\n');
 jsonFile = fullfile(tileDir,'dashboard.json');
 matFile = fullfile(tileDir,'dashboard.mat');
 if isempty(dir(matFile))
     %load JSON
-    fprintf('\nLoading Json file..');
+    fprintf('Loading Json file..\n');
     if isempty(dir(jsonFile)), error('JSON file: %s could not be found',jsonFile); end
     opt.ShowProgress = true;
     jsonData = loadjson(jsonFile,opt);
-    fprintf('\nSaving Json file..');
+    fprintf('Saving Json file..\n');
     save(matFile,'jsonData');
 else
     %load Mat
-    fprintf('\nLoading pre-parsed Json file..');
+    fprintf('Loading pre-parsed Json file..\n');
     load(matFile);
 end
-fprintf('\nDone');
+fprintf('Done\n');
 
 %% Get lattice position and file location all tiles.
 contents = structfun(@(x) x,jsonData.tileMap,'UniformOutput',false);
@@ -32,7 +34,35 @@ latPos = cellfun(@(x) x.contents.latticePosition,contents); % get Lattice positi
 latPos = [latPos.x;latPos.y;latPos.z]';
 fileLoc = cellfun(@(x) fullfile(jsonData.monitor.location,x.relativePath),contents,'UniformOutput',false)'; % get file position.
 
-a=1;
+%% Get rid of duplicates (use most recent)
+latMat = zeros(max(latPos(:,1)),max(latPos(:,2)),max(latPos(:,3)));
+for iTile = 1:size(latPos,1)
+    if latPos(iTile,1)>0
+        latMat(latPos(iTile,1),latPos(iTile,2),latPos(iTile,3)) = latMat(latPos(iTile,1),latPos(iTile,2),latPos(iTile,3)) +1;
+    end
+end
+% find lattice positions with duplicate entries.
+[x,y,z] = ind2sub(size(latMat),find(latMat>1));removeList = [];
+% Select earliest tile for removal.
+for iTile = 1:size(x,1)
+   ind = find(latPos(:,1)==x(iTile) &  latPos(:,2)==y(iTile) &  latPos(:,3)==z(iTile));
+   removeList = [removeList; min(ind)];
+end
+fileLoc(removeList,:) = [];
+latPos(removeList,:) = [];
+
+%% Get field of view info from protobuf file.
+ind = find(latPos(:,3)>0,1);
+FOV = fastProtoBuf( fullfile(fileLoc{ind},[fileLoc{ind}(end-4:end),'-ngc.microscope']), {'x_size_um','y_size_um','z_size_um','x_overlap_um','y_overlap_um','z_overlap_um'});
+FOV.x_size_pix = contents{ind}.contents.imageContents{1,1}.width;
+FOV.y_size_pix = contents{ind}.contents.imageContents{1,1}.height;
+FOV.x_res = FOV.x_size_um/FOV.x_size_pix;
+FOV.y_res = FOV.y_size_um/FOV.y_size_pix;
+
+
+%% Get general dimensions result file.
+fileLoc(latPos(:,3)==reqZ,:);
+latPos(latPos(:,3)==reqZ,:);
 
 end
 
